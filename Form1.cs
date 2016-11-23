513e6ad1629a31202b548ca0b4707373b35136eb
aot1 <aot1@students.uwf.edu>
2016-11-23 00:05:39 -0600

    Character and such.
    Character classes for game.

---
+++ Form1.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Character
+{
+    class Character
+    {
+        private int health;
+        private int attack;
+        private int defense;
+
+        public Character(int h, int a, int d)
+        {
+            
+        }
+
+        
+    }
+}
