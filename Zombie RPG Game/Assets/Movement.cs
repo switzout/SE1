c307325fda09981cd72f9c11cf67c785ee62b2a1
JLS <jlschwitzerlett@gmail.com>
2016-11-25 19:39:04 -0600

    Unity Project

---
+++ Zombie RPG Game/Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector2 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector2 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector2 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector2 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+    }
+}
